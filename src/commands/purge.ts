2f2ca699083ada8e30276f16c2f2c492ed137302
RomaDevWorld <50528338+RomaDevWorld@users.noreply.github.com>
2023-07-05 19:49:02 +0300

    Add: purge command

---
+++ src/commands/purge.ts
@@ -0,0 +1,61 @@
+import { ChannelType, EmbedBuilder, Message, PermissionFlagsBits, SlashCommandBuilder } from 'discord.js'
+import { SlashCommand } from '../../types'
+import { t } from 'i18next'
+
+const command: SlashCommand = {
+  command: new SlashCommandBuilder()
+    .setName('purge')
+    .setDescription('Delete large amount of messages')
+    .setDescriptionLocalizations({
+      uk: 'Видалення великої кількості повідомлень',
+    })
+    .addIntegerOption((option) =>
+      option
+        .setMinValue(0)
+        .setMaxValue(100)
+        .setName('amount')
+        .setRequired(true)
+        .setDescription('Max amount of messages')
+        .setDescriptionLocalizations({
+          uk: 'Максимальна кількість повідомлень',
+        })
+    )
+    .addUserOption((option) =>
+      option.setName('user').setDescription('Delete messages from a specific user').setDescriptionLocalizations({
+        uk: 'Видалення повідомлень від вказаного користувача',
+      })
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+  cooldown: 10,
+  execute: async (interaction) => {
+    const amount = interaction.options.getInteger('amount') as number
+    const user = interaction.options.getUser('user')
+
+    if (interaction.channel?.type !== ChannelType.GuildText) return console.error('[Error] Purge has been executed in wrong channel')
+
+    let messages = await interaction.channel?.messages.fetch({
+      limit: amount,
+    })
+    if (user) messages = messages?.filter((m) => m.author.id === user.id)
+
+    const agedMessages = messages?.filter((m) => m.createdTimestamp < Date.now() - 1000 * 60 * 60 * 24 * 14)
+
+    await interaction.channel?.bulkDelete(messages, true).then(async (messages) => {
+      const embed = new EmbedBuilder()
+        .setColor('Green')
+        .setTitle(t('purge_success_title', { lng: interaction.locale }))
+        .setDescription(t('purge_success_description', { lng: interaction.locale, messages: messages.size, agedMessages: agedMessages.size }))
+      await interaction.reply({ embeds: [embed], ephemeral: true })
+
+      let n = 1
+      agedMessages.forEach(async (message: Message) => {
+        setTimeout(async () => {
+          await message.delete()
+          n++
+        }, n * 1000)
+      })
+    })
+  },
+}
+
+export default command
