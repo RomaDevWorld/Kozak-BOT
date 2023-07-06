948ff517e42441b155077f3bff4dab7cb93ac1c0
Voxlinou <71925280+makouille@users.noreply.github.com>
2023-07-06 22:43:30 +0100

    Added french

--- src/locales/resources.ts
+++ src/locales/resources.ts
@@ -1,11 +1,14 @@
 import common_en from '../locales/en/common.json'
 import common_uk from '../locales/uk/common.json'
+import common_fr from '../locales/fr/common.json'
 
 import eightball_en from '../locales/en/8ball.json'
 import eightball_uk from '../locales/uk/8ball.json'
+import eightball_fr from '../locales/fr/8ball.json'
 
 import dice_en from '../locales/en/dice.json'
 import dice_uk from '../locales/uk/dice.json'
+import dice_fr from '../locales/fr/dice.json'
 
 const resources = {
   en: {
@@ -17,6 +20,11 @@ const resources = {
     common: common_uk,
     '8ball': eightball_uk,
     dice: dice_uk,
+  },
+  fr: {
+    common: common_fr,
+    '8ball': eightball_fr,
+    dice: dice_fr,
   },
 }
 
