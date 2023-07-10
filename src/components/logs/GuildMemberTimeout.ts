332b5d401afe57dbb0f8288f13aeff50b9ac1dfb
RomaDevWorld <50528338+RomaDevWorld@users.noreply.github.com>
2023-07-10 22:50:55 +0300

    Add: Fetch audit in GuildMemberTimeout.ts

--- src/components/logs/GuildMemberTimeout.ts
+++ src/components/logs/GuildMemberTimeout.ts
@@ -1,6 +1,7 @@
-import { EmbedBuilder, GuildMember } from 'discord.js'
+import { AuditLogEvent, EmbedBuilder, GuildMember } from 'discord.js'
 import validateLog from '../../functions/validateLog'
 import { t } from 'i18next'
+import fetchAuditLog from '../../functions/fetchAuditLog'
 
 const GuildMemberTimeoutLog = async (oldMember: GuildMember, newMember: GuildMember) => {
   if (newMember.user.bot) return
@@ -10,37 +11,54 @@ const GuildMemberTimeoutLog = async (oldMember: GuildMember, newMember: GuildMem
   const channel = await validateLog(newMember.guild, 'guildMemberTimeout')
   if (!channel) return
 
+  let audit = await fetchAuditLog(newMember.guild, AuditLogEvent.MemberUpdate)
+  if (audit?.targetId !== newMember.id || audit?.changes[0].key !== 'communication_disabled_until') audit = undefined
+
+  const lng = newMember.guild.preferredLocale
+
+  console.log(audit)
+
   if (oldMember.communicationDisabledUntil && !newMember.communicationDisabledUntil) {
     const embed = new EmbedBuilder()
       .setColor('Green')
       .setAuthor({
-        name: t('logs:guildMemberTimeout_unmute_author', { lng: newMember.guild.preferredLocale, user: newMember.user.username }),
+        name: t('logs:guildMemberTimeout_unmute_author', { lng, user: newMember.user.username }),
         iconURL: newMember.user.displayAvatarURL(),
       })
       .setFooter({ text: newMember.id })
-      .addFields({ name: t('member', { lng: newMember.guild.preferredLocale }), value: newMember.toString() })
+      .addFields({ name: t('member', { lng }), value: newMember.toString() })
       .setTimestamp()
+    if (audit)
+      embed.addFields(
+        { name: t('moderator', { lng }), value: audit.executor?.toString() || 'N/A' },
+        { name: t('reason', { lng }), value: audit.reason || t('reasonNotSpecified', { lng }) }
+      )
 
     channel.send({ embeds: [embed] })
   } else if (newMember.communicationDisabledUntil && !oldMember.communicationDisabledUntil) {
     const embed = new EmbedBuilder()
       .setColor('Red')
       .setAuthor({
-        name: t('logs:guildMemberTimeout_mute_author', { lng: newMember.guild.preferredLocale, user: newMember.user.username }),
+        name: t('logs:guildMemberTimeout_mute_author', { lng, user: newMember.user.username }),
         iconURL: newMember.user.displayAvatarURL(),
       })
       .setFooter({ text: newMember.id })
       .addFields(
         {
-          name: t('member', { lng: newMember.guild.preferredLocale }),
+          name: t('member', { lng }),
           value: newMember.toString(),
         },
         {
-          name: t('time', { lng: newMember.guild.preferredLocale }),
+          name: t('time', { lng }),
           value: new Date(newMember.communicationDisabledUntilTimestamp as number).toLocaleString(),
         }
       )
       .setTimestamp()
+    if (audit)
+      embed.addFields(
+        { name: t('moderator', { lng }), value: audit.executor?.toString() || 'N/A' },
+        { name: t('reason', { lng }), value: audit.reason || t('reasonNotSpecified', { lng }) }
+      )
     channel.send({ embeds: [embed] })
   }
 }
