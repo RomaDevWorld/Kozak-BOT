6ad7f6ad74c4f6f0b7c7d12b3dabb9f9168dea22
RomaDevWorld <50528338+RomaDevWorld@users.noreply.github.com>
2023-07-10 22:38:41 +0300

    Add: Fetch audit in guildMemberNicknameUpdate

--- src/components/logs/GuildMemberNicknameUpdate.ts
+++ src/components/logs/GuildMemberNicknameUpdate.ts
@@ -1,6 +1,7 @@
-import { EmbedBuilder, GuildMember } from 'discord.js'
+import { AuditLogEvent, EmbedBuilder, GuildMember } from 'discord.js'
 import validateLog from '../../functions/validateLog'
 import { t } from 'i18next'
+import fetchAuditLog from '../../functions/fetchAuditLog'
 
 const GuildMemberNicknameUpdateLog = async (oldMember: GuildMember, newMember: GuildMember) => {
   if (newMember.user.bot) return
@@ -10,23 +11,25 @@ const GuildMemberNicknameUpdateLog = async (oldMember: GuildMember, newMember: G
   const channel = await validateLog(newMember.guild, 'guildMemberNicknameUpdate')
   if (!channel) return
 
+  const lng = newMember.guild.preferredLocale
+
   const embed = new EmbedBuilder()
     .setAuthor({
-      name: t('logs:guildMemberNicknameUpdate_author', { lng: newMember.guild.preferredLocale, user: newMember.user.username }),
+      name: t('logs:guildMemberNicknameUpdate_author', { lng, user: newMember.user.username }),
       iconURL: newMember.displayAvatarURL(),
     })
     .addFields(
       {
-        name: t('member', { lng: newMember.guild.preferredLocale }),
+        name: t('member', { lng }),
         value: newMember.toString(),
       },
       {
-        name: t('before', { lng: newMember.guild.preferredLocale }),
+        name: t('before', { lng }),
         value: oldMember.nickname || 'N/A',
         inline: true,
       },
       {
-        name: t('now', { lng: newMember.guild.preferredLocale }),
+        name: t('now', { lng }),
         value: newMember.nickname || 'N/A',
         inline: true,
       }
@@ -34,6 +37,12 @@ const GuildMemberNicknameUpdateLog = async (oldMember: GuildMember, newMember: G
     .setFooter({ text: `ID: ${newMember.id}` })
     .setColor('Blue')
     .setTimestamp()
+
+  const audit = await fetchAuditLog(newMember.guild, AuditLogEvent.MemberUpdate)
+  if (audit && audit.targetId === newMember.id) {
+    embed.addFields({ name: t('executor', { lng }), value: audit.executor?.toString() || 'N/A' })
+    if (audit.reason) embed.addFields({ inline: true, name: t('reason', { lng }), value: audit.reason || 'N/A' })
+  }
 
   channel.send({ embeds: [embed] })
 }
