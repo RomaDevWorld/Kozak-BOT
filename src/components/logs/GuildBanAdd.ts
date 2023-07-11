1cc9a95c9b09aa50af7edbd13aa0b7492275983f
RomaDevWorld <50528338+RomaDevWorld@users.noreply.github.com>
2023-07-11 15:41:05 +0300

    Add: Fetch audit in banAdd & banRemove

--- src/components/logs/GuildBanAdd.ts
+++ src/components/logs/GuildBanAdd.ts
@@ -1,20 +1,29 @@
-import { EmbedBuilder, GuildBan } from 'discord.js'
+import { AuditLogEvent, EmbedBuilder, GuildBan } from 'discord.js'
 import validateLog from '../../functions/validateLog'
 import { t } from 'i18next'
+import fetchAuditLog from '../../functions/fetchAuditLog'
 
 const GuildBanAddLog = async (guildBan: GuildBan) => {
   const channel = await validateLog(guildBan.guild, 'guildBanAdd')
   if (!channel) return
 
+  const lng = guildBan.guild.preferredLocale
+
   const embed = new EmbedBuilder()
     .setAuthor({
-      name: t('logs:guildBanAdd_author', { lng: guildBan.guild.preferredLocale, user: guildBan.user.username }),
+      name: t('logs:guildBanAdd_author', { lng, user: guildBan.user.username }),
       iconURL: guildBan.user.displayAvatarURL(),
     })
     .setDescription(`${guildBan.user} (${guildBan.user.username})`)
     .setFooter({ text: `ID: ${guildBan.user.id}` })
     .setColor('Red')
     .setTimestamp()
+
+  const audit = await fetchAuditLog(guildBan.guild, AuditLogEvent.MemberBanAdd)
+  if (audit && audit.targetId === guildBan.user.id) {
+    embed.addFields({ name: t('moderator', { lng }), value: audit.executor?.toString() || 'N/A' })
+    if (audit.reason) embed.addFields({ inline: true, name: t('reason', { lng }), value: audit.reason })
+  }
 
   channel.send({ embeds: [embed] })
 }
