fdfe35827fa572b5918b8007a00007e675c08054
RomaDevWorld <50528338+RomaDevWorld@users.noreply.github.com>
2023-07-19 22:08:34 +0300

    Add: Auto ban member on reaction

---
+++ src/events/raw.ts
@@ -0,0 +1,45 @@
+import { TextChannel, MessageReaction, User } from 'discord.js'
+import { BotEvent } from '../@types/discord'
+import client from '../lib/client'
+
+interface RawEventData {
+  t: string
+  d: {
+    user_id: string
+    channel_id: string
+    message_id: string
+    emoji: {
+      id: string | null
+      name: string
+    }
+  }
+}
+
+const event: BotEvent = {
+  name: 'raw',
+  once: false,
+  execute: async (event: RawEventData) => {
+    const events = {
+      MESSAGE_REACTION_ADD: 'messageReactionAdd',
+      MESSAGE_REACTION_REMOVE: 'messageReactionRemove',
+    }
+
+    if (!(event.t in events)) return
+
+    const { d: data } = event
+    const user = client.users.cache.get(data.user_id) as User | undefined
+    const channel = client.channels.cache.get(data.channel_id) as TextChannel | undefined
+    if (!channel) return
+
+    if (channel.messages.cache.has(data.message_id)) return
+
+    const message = await channel.messages.fetch(data.message_id)
+
+    const emojiKey = data.emoji.id ? `${data.emoji.name}:${data.emoji.id}` : data.emoji.name
+    const reaction = message.reactions.cache.get(emojiKey) as MessageReaction | undefined
+
+    client.emit(events[event.t as keyof typeof events], reaction, user)
+  },
+}
+
+export default event
--- src/lib/client.ts
+++ src/lib/client.ts
@@ -13,6 +13,7 @@ const client = new Client({
     GatewayIntentBits.GuildVoiceStates,
     GatewayIntentBits.GuildModeration,
     GatewayIntentBits.GuildInvites,
+    GatewayIntentBits.GuildMessageReactions,
   ],
 })
 
