dd1a94f840801a86b87168f0d9ea3e3f1345947e
RomaDevWorld <50528338+RomaDevWorld@users.noreply.github.com>
2023-07-21 22:39:41 +0300

    Add: Ticket logs (incomplete)

--- src/components/buttons/CloseTicket.ts
+++ src/components/buttons/CloseTicket.ts
@@ -1,10 +1,12 @@
-import { ButtonBuilder, ButtonStyle, TextChannel } from 'discord.js'
+import { AttachmentBuilder, ButtonBuilder, ButtonStyle, TextChannel } from 'discord.js'
 import { Button } from '../../@types/discord'
 import { t } from 'i18next'
+import { createWriteStream, existsSync, mkdirSync, unlink } from 'fs'
+import moment from 'moment'
 
 const CloseTicket: Button = {
   button: new ButtonBuilder().setCustomId('close_ticket').setEmoji('🔒').setStyle(ButtonStyle.Danger),
-  execute(interaction) {
+  async execute(interaction) {
     const channel = interaction.channel as TextChannel
     if (!channel) return
 
@@ -13,10 +15,45 @@ const CloseTicket: Button = {
         SendMessages: false,
       })
     )
-    channel.setName(`archive-` + channel.name)
+    // channel.setName(`archive-` + channel.name)
 
-    interaction.message.edit({ components: [] })
-    interaction.reply(t('ticketClosed', { lng: interaction.guild?.preferredLocale, member: interaction.user.toString() }))
+    // interaction.message.edit({ components: [] })
+
+    const messages = await channel.messages.fetch()
+    const ticketCreatedAt = messages.last()?.createdTimestamp
+
+    const messageHistory = messages
+      .reverse()
+      .filter((m) => !m.author.bot)
+      .map((m) => {
+        return (
+          `${m.author.username} (ID: ${m.author.id}): ${m.content}\n` + `${m.attachments.size > 0 ? m.attachments.map((i) => i.url).join('\n') : ''}`
+        )
+      })
+      .join('\n')
+
+    if (messageHistory.length <= 0) return
+
+    const cacheDir = './.cache'
+    if (!existsSync(cacheDir)) mkdirSync(cacheDir)
+
+    const filePath = `./.cache/${Date.now()}.log`
+
+    const stream = createWriteStream(filePath)
+
+    stream.write(`[Start ${moment(ticketCreatedAt).format(`DD.MM.YYYY HH:mm`)}]\n`)
+    stream.write(messageHistory)
+    stream.write(`\n[Close ${moment(Date.now()).format(`DD.MM.YYYY HH:mm`)} by user ${interaction.user.username} (ID: ${interaction.user.id})]`)
+    stream.end()
+
+    const attachment = new AttachmentBuilder(stream.path, { name: `ticket.log` })
+
+    interaction.reply({
+      content: t('ticketClosed', { lng: interaction.guild?.preferredLocale, member: interaction.user.toString() }),
+      files: [attachment],
+    })
+
+    unlink(filePath, (err) => console.error(err))
   },
 }
 
