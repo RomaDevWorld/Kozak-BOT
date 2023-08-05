dbe2609f04f73d5a82b4b92220cf0181bcb05da5
RomaDevWorld <50528338+RomaDevWorld@users.noreply.github.com>
2023-08-05 21:39:28 +0300

    Update: Extract private manage subcommands

---
+++ src/components/subCommands/privateManagePublic.ts
@@ -0,0 +1,28 @@
+import { GuildMember, PermissionFlagsBits, SlashCommandSubcommandBuilder } from 'discord.js'
+import { SubCommand } from '../../@types/discord'
+import { getPrivateChannel } from '../../functions/usePrivateChannel'
+import { t } from 'i18next'
+
+const PrivateManagePublicSubcommand: SubCommand = {
+  data: new SlashCommandSubcommandBuilder()
+    .setName('public')
+    .setDescription('Make the channel public or private')
+    .setDescriptionLocalizations({ uk: 'Зробити канал відкритим або особистим' }),
+  execute: function (interaction) {
+    const lng = interaction.locale
+
+    const channel = getPrivateChannel(interaction.member as GuildMember)
+    if (!channel) return interaction.reply({ content: t('private:noChannel', { lng }), ephemeral: true })
+    if (!interaction.guild) return
+
+    const isOn = channel.permissionsFor(interaction.guild?.id)?.has(PermissionFlagsBits.ViewChannel) ?? false
+
+    channel.permissionOverwrites.edit(interaction.guild?.id, {
+      ViewChannel: !isOn,
+    })
+
+    interaction.reply({ content: t('private:publicUpdated', { lng }), ephemeral: true })
+  },
+}
+
+export default PrivateManagePublicSubcommand
