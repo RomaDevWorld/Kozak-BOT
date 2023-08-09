b160d7110a6e9081b3bc95419029a3d17bc0f943
RomaDevWorld <50528338+RomaDevWorld@users.noreply.github.com>
2023-08-09 23:07:07 +0300

    Fix: parseMessageAttachment.ts

--- src/functions/parseMessageAttachments.ts
+++ src/functions/parseMessageAttachments.ts
@@ -3,7 +3,7 @@ import { Attachment, Collection } from 'discord.js'
 const parseMessageAttachments = (attachments: Collection<string, Attachment>) => {
   if (attachments.size < 1) return
 
-  return attachments.size > 1 ? attachments.map((i, index) => `${index}. ${i.url}`).join('\n') : attachments.first()?.url
+  return attachments.size > 1 ? attachments.map((i) => `${i.url}`).join('\n') : attachments.first()?.url
 }
 
 export default parseMessageAttachments
