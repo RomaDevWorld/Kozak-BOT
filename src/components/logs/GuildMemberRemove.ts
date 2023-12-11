713d4d8d8ac62f45fa1eaae39221c297c2e22e82
RomaDevWorld <50528338+RomaDevWorld@users.noreply.github.com>
2023-12-11 19:28:49 +0200

    feat: member join timestamp on leave

--- src/components/logs/GuildMemberRemove.ts
+++ src/components/logs/GuildMemberRemove.ts
@@ -2,8 +2,11 @@ import { AuditLogEvent, EmbedBuilder, GuildMember, ImageURLOptions } from 'disco
 import validateLog from '../../functions/validateLog'
 import { t } from 'i18next'
 import fetchAuditLog from '../../functions/fetchAuditLog'
+import timestamp from '../../functions/createTimestamp'
 
 const GuildMemberRemoveLog = async (member: GuildMember) => {
+  if (!member) return
+
   const isBanned = member.guild.bans.cache.find((ban) => ban.user.id === member.id)
   if (isBanned) return
 
@@ -14,13 +17,16 @@ const GuildMemberRemoveLog = async (member: GuildMember) => {
 
   const embed = new EmbedBuilder()
     .setAuthor({
-      name: t('logs:guildMemberRemove.undefined_author', { lng, user: member.user.username }),
+      name: t('logs:guildMemberRemove.unknown_author', { lng, user: member.user.username }),
       iconURL: member.displayAvatarURL({ dynamic: true } as ImageURLOptions),
     })
+
     .setDescription(`${member} (${member.user.username})`)
     .setFooter({ text: `ID: ${member.id}` })
     .setColor('Orange')
     .setTimestamp()
+  if (member.joinedTimestamp)
+    embed.addFields({ name: t('logs:guildMemberRemove.joinedAt', { lng }), value: `${timestamp(member.joinedTimestamp, 'f')}` })
 
   const audit = await fetchAuditLog(member.guild, AuditLogEvent.MemberKick)
 
--- src/functions/createTimestamp.ts
+++ src/functions/createTimestamp.ts
@@ -1,3 +1,18 @@
+/**
+ * Formats a timestamp using Discord's timestamp format.
+ *
+ * @param {number|string} timestamp - The unix timestamp to format
+ * @param {'t' | 'T' | 'd' | 'D' | 'f' | 'F' | 'R'} [format='t'] - The format to use for the timestamp.
+ *   - 't': Short time format (09:01)
+ *   - 'T': Long time format (09:01:00)
+ *   - 'd': Short date format (28/11/2018)
+ *   - 'D': Long date format (28 November 2018)
+ *   - 'f': Short date and time format (28 November 2018 09:01)
+ *   - 'F': Long date and time format (Wednesday, 28 November 2018 09:01)
+ *   - 'R': Relative time format (3 years ago)
+ *
+ * @returns {string} The formatted timestamp string.
+ */
 const timestamp = (timestamp: number | string, format?: 't' | 'T' | 'd' | 'D' | 'f' | 'F' | 'R') => {
   return `<t:${timestamp.toString().slice(0, 10)}:${format}>`
 }
