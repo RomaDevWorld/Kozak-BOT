afcf60303785cd831198e9f0eff8fa6ea218670e
RomaDevWorld <50528338+RomaDevWorld@users.noreply.github.com>
2024-04-07 08:54:53 +0000

    style: starboard message

--- src/functions/useStarboard.ts
+++ src/functions/useStarboard.ts
@@ -1,5 +1,4 @@
 import { ChannelType, EmbedBuilder, ImageURLOptions, MessageReaction, User, parseEmoji } from 'discord.js'
-import { t } from 'i18next'
 import Modules from '../schemas/Modules'
 import StarboardMessages from '../schemas/StarboardMessages'
 
@@ -8,8 +7,6 @@ export const handleStarReaction = async (reaction: MessageReaction, user: User) 
 
   const guild = reaction.message.guild
   if (!guild) return
-  const lng = guild.preferredLocale
-
   const count = reaction.users.cache.filter((user) => !user.bot).size
   if (count < 3) return
 
@@ -39,14 +36,12 @@ export const handleStarReaction = async (reaction: MessageReaction, user: User) 
 
   const embed = new EmbedBuilder()
     .setColor('Yellow')
-    .setDescription(`${reaction.message.content?.slice(0, 4000)}\n\n[${t('logs:messageUpdate.title', { lng })}](${reaction.message.url})`)
+    .setTimestamp()
+    .setDescription(`${reaction.message.content?.slice(0, 4000)}\n\n${reaction.message.url}`)
     .setAuthor({
       name: reaction.message.author?.username || 'N/A',
       iconURL: reaction.message.author?.avatarURL({ dynamic: true } as ImageURLOptions) || undefined,
     })
-    .setTimestamp()
-    .setFooter({ text: reaction.message.id })
-    .addFields({ name: t('channel_one', { lng }), value: reaction.message.channel.toString() })
   if (reaction.message.attachments.size > 0) embed.setImage(reaction.message.attachments.first()?.url || null)
 
   const starboardMessage = await channel.send({ content: `**${reaction.emoji.toString()} ${count}**`, embeds: [embed] }).catch((e) => e)
